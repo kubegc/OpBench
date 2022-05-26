bd96e6dff74ffcca92b84d45f3270105809d6932
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2022-05-26 20:27:51 +0800

    bad

--- python/relay_profiler/relay_analyzer.py
+++ python/relay_profiler/relay_analyzer.py
@@ -2,6 +2,11 @@ import util
 import model_importer.local_nns as local_nns
 import tvm
 
+target = tvm.target.Target("cuda")
+dev = tvm.device(str(target), 0)
 mod, params, input_shape, output_shape,inputs = local_nns.get_network("resnet-18")
-lib = util.compile_without_log(mod, tvm.target.Target("cuda"),params)
-util.analyze_relay_json(lib.get_graph_json())
+lib = util.compile_without_log(mod, target, params)
+# util.analyze_relay_json(lib.get_graph_json())
+print(type(lib))
+
+print(type(lib["default"](dev)))
