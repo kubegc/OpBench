56512ffe5a8ff78b5168b81311a17fff5b2a6baa
Xuyuanjia2014 <xuyuanjia2017@otcaix.iscas.ac.cn>
2022-09-06 15:42:27 +0800

    update op_costs

--- test/test_op_costs.py
+++ test/test_op_costs.py
@@ -1,6 +1,9 @@
 from tvm.autotvm.record import decode
 import numpy as np
 import pandas as pd
+import argparse
+import shutil
+import os
 # 测量算子在100,500,1000此迭代中运行时间和最优时间的距离，假定3000次得到的时间为最优解
 # batch_
 
@@ -17,23 +20,32 @@ def parse_logs(file_path):
     return inps, results
 
 if __name__ == '__main__':
-    # parser = argparse.Argumentparser()
-    # parser.add_argument("--modelname", type=str, default=none)
-    # parser.add_argument("--tuner", type=str, default=none)
-    # parser.add_argument("--target", type=str, default=none)
-    # args = parser.parse_args()
-    modelname = "bert"
-    tuner = "xgb_knob"
-    target = "cuda"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--modelname", type=str, default=None)
+    parser.add_argument("--tuner", type=str, default="random")
+    parser.add_argument("--target", type=str, default="cuda")
+    args = parser.parse_args()
+    modelname = args.modelname
+    tuner = args.tuner
+    target = args.target
+    #  python test/test_op_costs.py --modelname=resnet-18 --target=cuda --tuner=resnet-18
+    # modelname = "resnet-18"
+    # tuner = "xgb_knob"
+    # target = "cuda"
+    data_path = "/root/github/OpBench/exp/op_costs/"+modelname+"_"+tuner+"_"+target
     log = "/root/github/OpBench/data/Performance/"+modelname+ '-' + tuner +"-"+target+"-autotvm.json"
     tmp_log = log + ".tmp"
     inps, results = parse_logs(tmp_log)
     best_inps, best_results = parse_logs(log)
-    df = pd.DataFrame(columns=["op_index", "task_name", "task_args","trial", "costs", "all_cost", "best_cost", "best_all_cost", "cmp"])
+    if os.path.exists(data_path):
+        shutil.rmtree(data_path)
+    os.mkdir(data_path)
+    
     for op_index in range(len(best_inps)):
         inp = best_inps[op_index]
         res = best_results[op_index]
         trial = 0
+        df = pd.DataFrame(columns=[ "task_name", "trial", "costs", "all_cost", "best_cost", "best_all_cost", "cmp"])
         # 筛选出一个算子的调优记录
         for i in range(len(inps)):
             tmp_inp = inps[i]
@@ -41,19 +53,21 @@ if __name__ == '__main__':
             if(inp.task.name==tmp_inp.task.name and inp.task.args == tmp_inp.task.args):
                 trial += 1
                 item = {
-                   "op_index":op_index,
                    "task_name":tmp_inp.task.name,
-                   "task_args": tmp_inp.task.args,
                    "trial":trial,
                    "costs": np.mean(tmp_res.costs),
                    "all_cost": tmp_res.all_cost,
                    "best_cost": np.mean(res.costs),
                    "best_all_cost": res.all_cost,
                     "cmp": (np.log(np.mean(tmp_res.costs)))/(np.log(np.mean(res.costs)))
-                }
+                    # "cmp": np.exp(np.mean(res.costs)-tmp_res.costs)                
+                    }
                 if tmp_res.error_no == 0:
                     # item["costs"] = 0.0
                     # item["cmp"] = 0.0
                     df = df.append(item, ignore_index=True)
-    print(df.size)
-    df.to_csv("/root/github/OpBench/exp/op_costs/"+modelname+"_"+tuner+"_"+target+"_costs.csv", index=False)
+        print(df.size)
+        worst_cost = df["costs"].max()
+        best_cost = df["costs"].min()
+        df["cmp"] = (worst_cost-df["costs"])/(worst_cost-best_cost)
+        df.to_csv(data_path+"/"+str(op_index)+".csv",index=False)
